4cc762e22e541f7f153e673013581e5f22cea2be
Junya Sugihara <hokkaru3@gmail.com>
2020-01-07 18:25:02 +0100

    resize avatar & add partial   close #10   close #22

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,4 +1,24 @@
 # frozen_string_literal: true
 
 module UsersHelper
+
+  # def display_user_avatar(user, size)
+  #   link_to user_path(user) do
+  #     if user.avatar.attached?
+  #       image_tag user.avatar, size: size, class: 'avatar'
+  #     else
+  #       image_tag 'default_avatar', size: size, class: 'avatar'
+  #     end
+  #   end
+  # end
+
+  def display_user_avatar(user, width, height)
+    link_to user_path(user) do
+      if user.avatar.attached?
+        image_tag user.avatar.variant(combine_options: { resize: "#{width}x#{height}^", crop: "#{width}x#{height}+0+0", gravity: :center }).processed, class: 'avatar'
+      else
+        image_tag 'default_avatar', size: "#{width}x#{height}", class: 'avatar'
+      end
+    end
+  end
 end
