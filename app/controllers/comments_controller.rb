ccb24cec4ed39aa6494b2cb814c47d07460c1b04
Junya Sugihara <hokkaru3@gmail.com>
2020-01-11 14:47:20 +0100

    install i18n and change some error messages

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,12 +4,14 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
+    flash[:success] = "コメントを投稿しました"
     redirect_to post_path(@post)
   end
 
   def destroy
     comment = current_user.comments.find_by(post_id: params[:post_id], id: params[:id])
     comment.destroy
+    flash[:success] = "コメントを削除しました"
     redirect_to post_path(params[:post_id])
   end
 
