97ae2d3fa91ade3d18a6a8fcf0b3bc2c785d57f7
hotcalpis <hokkaru3@gmail.com>
2020-01-16 15:51:51 +0000

    gmail setting for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,11 +71,11 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer_sender = ENV['GMAIL_USERNAME']
+  config.action_mailer.default_url_options = { host: "18.177.37.87" }
   config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.net',
+    address: 'smtp.gmail.com',
     port: 587,
-    authentication: :plain,
+    authentication: :login,
     user_name: ENV['GMAIL_USERNAME'],
     password: ENV['GMAIL_PASSWORD'],
     enable_starttls_auto: true
