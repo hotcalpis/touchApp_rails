53d816a3f6e5fed199b0b4e1f03d4d570e610508
Junya Sugihara <hokkaru3@gmail.com>
2020-01-25 12:32:44 +0900

    omniauth google

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -14,10 +14,12 @@
 #  github                 :string(255)
 #  name                   :string(255)      default(""), not null
 #  profile                :text(65535)
+#  provider               :string(255)
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string(255)
 #  twitter                :string(255)
+#  uid                    :string(255)
 #  unconfirmed_email      :string(255)
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
